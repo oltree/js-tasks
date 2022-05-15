121041b0b0953a5f48c457944e60f543e3fbedb7
oltree <o.meleh10@gmail.com>
2022-05-15 19:57:59 +0300

    commit

--- index.js
+++
@@ -1,9 +0,0 @@
-
-const showMessage = function(value) {
-	if (value > 10) return "error, number can not be grater than 10";
-	if (value == 5) return "It's middle";
-	if (value == 1) return "Lower middle";
-	if (value == 6) return "Upper middle";
-	if (value == 10) return "The biggest";
-}
-console.log(showMessage(1));
