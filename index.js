6799ee245e69dcde865c2b51c16b7399e6a5aad6
oltree <o.meleh10@gmail.com>
2022-05-22 12:30:46 +0300

    HW №2  TMS JS

--- index.js
+++ index.js
@@ -1,84 +1,156 @@
-//task №9
-/* const getResult = function (string, number) {
-	return string.length > number ? 'String is too long!' : string;
-};
-console.log(getResult(prompt('Введите строку!'), prompt('Введите число!')));  */
+//task №1
 
-/* const getResult = (string, number) => string.length > number ? 'String is too long!' : string;
-console.log(getResult(prompt('Введите строку!'), prompt('Введите число!'))); */
+const arrayNumbers = [1 , 2, 3, 4, 5, 6, 7, 8];
 
+let convertNumbersInArray = (arrayNumbers) => {
+	return arrayNumbers.map((number) => (number % 2 === 0) ? number += 1 : number -= 1);
+}
 
-//task №8
-/* let stringOne = String(prompt('Введит первую строку!'));
-let stringTwo = String(prompt('Введите вторую строку!'));
-console.log(`${stringOne} ${stringTwo}`); */
-
-
-//task №7
-/* const showMassage = function (letter) {
-	return letter === letter.toUpperCase() ? 'Оууу май, большая буква!' : 'Нет уж, маленькие буквы - скучно';
-};
-console.log(showMassage(prompt('Введите букву!'))); */
-
-/* const showMassage = (letter) => letter === letter.toUpperCase() ? 'Оууу май, большая буква!' : 'Нет уж, маленькие буквы - скучно';
-console.log(showMassage(prompt('Введите букву!'))); */
-
-
-//task №6
-/* const newString = function (str) {
-	return str[0].toUpperCase() + str.slice(1).toLowerCase();
-};
-console.log(newString(prompt('Введите строку!'))); */
-
-/* const newString = (str) => str[0].toUpperCase() + str.slice(1).toLowerCase();
-console.log(newString(prompt('Введите строку!'))); */
-
-
-//task №5
-/* const deleteChars = function (str) {
-	return str.slice(1, -1);
-};
-console.log(deleteChars(prompt('Введите строку!'))); */
-
-/* const deleteChars = (str) => str.slice(1, -1);
-console.log(deleteChars(prompt('Введите строку!'))); */
-
-
-//task №4
-/* const getResult = function (number) {
-	return number % 2 === 0;
-};
-console.log(getResult(prompt('Введите число!'))); */
-
-/* const getResult = (number) => number % 2 === 0;
-console.log(getResult(prompt('Введите число!'))); */
-
-
-//task №3
-/* const getMinResult = function (a, b) {
-	return a < b ? a : b;
-};
-console.log(getMinResult(prompt('Введите первое число!'), prompt('Введите второе число!'))); */
-
-/* const getMinResult = (a, b) => a < b ? a : b;
-console.log(getMinResult(prompt('Введите первое число!'), prompt('Введите второе число!'))); */
+console.log(convertNumbersInArray(arrayNumbers));
 
 
 //task №2
-/* let numberOne = Number(prompt('Введите сторону треугольника!'));
-let numberTwo = Number(prompt('Введите сторону треугольника!'));
-console.log(`Длинна гипотенузы: ${Math.pow((Math.pow(numberOne, 2) + Math.pow(numberTwo, 2)), 1 / 2)}`); */
+
+const arrayStrings = ['hello', 'wOrLd'];
+let convertStringsInArray = (arrayStrings) => {
+	return arrayStrings.map((string) => string[0].toUpperCase() + string.slice(1).toLowerCase());
+}
+
+console.log(convertStringsInArray(arrayStrings));
 
 
-//task №1
-/* const showMessage = function (name) {
-	const greetingWord = name.toLowerCase() === 'mark' ? 'Hi' : 'Hello';
-	return `${greetingWord}, ${name}!`
-};
-console.log(showMessage(prompt('Введите имя!'))); */
+//task №3
 
-/* const showMessage = (name) => {
-	const greetingWord = name.toLowerCase() === 'mark' ? 'Hi' : 'Hello';
-	return `${greetingWord}, ${name}!`
-};
-console.log(showMessage(prompt('Введите имя!'))); */
+const usersList = [
+	{
+		id: 1,
+		name: 'Alex',
+	},
+	{
+		id: 2,
+		name: 'Toma',
+	},
+	{
+		id: 3,
+		name: 'Max',
+	},
+]
+
+let showUsers = (usersList, id) => {
+	return usersList.filter((user) => {
+		return !id.includes(user.id);
+	});
+}
+
+console.log(showUsers(usersList, [1]));
+
+
+//task №4
+
+let forCheckPalindrome = (value) => value.split('').reverse().join('') === value;
+
+console.log(forCheckPalindrome('112211'));
+
+
+//task №5
+const users = [
+	{
+		id: 1,
+		first_name: 'Jeanette',
+		last_name: 'Penddreth',
+		email: 'jpenddreth0@census.gov',
+		gender: 'Female',
+		ip_address: '26.58.193.2',
+	},
+	{
+		id: 2,
+		first_name: 'Petr',
+		last_name: 'Jackson',
+		email: 'gfrediani1@senate.gov',
+		gender: 'Male',
+		ip_address: '229.179.4.212',
+	},
+];
+
+let convertUsersNewArray = (users) => {
+	return users.map((user) => {
+		return {
+			id: user.id,
+			fulName: `${user.first_name} ${user.last_name}`,
+			email: user.email,
+			gender: user.gender,
+			ip_address: user.ip_address,
+		}
+	});
+}
+
+let convertUsersNewArrayWomen = (users) => {
+	return users.filter((user) => user.gender.toLowerCase() === 'female');
+}
+
+let convertUsersNewArrayMan = (users) => {
+	return users.filter((user) => user.gender.toLowerCase() === 'male');
+}
+
+let getResult = (users) => {
+	return {
+		women: convertUsersNewArrayWomen(convertUsersNewArray(users)),
+		man: convertUsersNewArrayMan(convertUsersNewArray(users)),
+	}
+}
+console.log(getResult(users));
+
+
+//task №6
+const newReleases = [
+	{
+		id: 70111470,
+		title: 'Die Hard',
+		boxart: 'https://learn.javascript.ru/object1111',
+		uri: 'https://learn.javascript.ru/object2222',
+		rating: [5.0],
+		bookmark: [],
+	},
+	{
+		id: 1561615454,
+		title: 'Bad Boys',
+		boxart: 'https://learn.javascript.ru/object1111',
+		uri: 'https://learn.javascript.ru/object2222',
+		rating: [4.0],
+		bookmark: [{id: 23123123, time: 1232132}],
+	},
+	{
+		id: 7011142323270,
+		title: 'The Chamber',
+		boxart: 'https://learn.javascript.ru/object1111',
+		uri: 'https://learn.javascript.ru/object2222',
+		rating: [4.0],
+		bookmark: [],
+	},
+	{
+		id: 8729789470,
+		title: 'Fracture',
+		boxart: 'https://learn.javascript.ru/object1111',
+		uri: 'https://learn.javascript.ru/object2222',
+		rating: [5.0],
+		bookmark: [{id: 3123, time:32332}],
+	},
+];
+
+let convertUsers = (newReleases) => {
+	return newReleases.map((release) => {
+		return {
+			id: release.id, 
+			title: release.title,
+		};
+	});
+}
+console.log(convertUsers(newReleases));
+
+
+//task №7
+
+let ids = (newReleases) => {
+	return newReleases.filter(release => release.rating.includes(5.0));
+}
+console.log(ids(newReleases));
