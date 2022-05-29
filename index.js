0d52acb1ccee72d995a62eb157247c9b00d5cd61
oltree <o.meleh10@gmail.com>
2022-05-29 23:04:11 +0300

    HW 6

--- index.js
+++ index.js
@@ -1,149 +1,201 @@
-//task №1
+//Task №1
 
-const arrayNumbers = [1 , 2, 3, 4, 5, 6, 7, 8];
-
-let isEven = (arrayNumbers) => {
-	return arrayNumbers.map((number) => (number % 2 === 0) ? number + 1 : number - 1);
-}
-
-console.log(isEven(arrayNumbers));
-
-
-//task №2
-
-const arrayStrings = ['hello', 'wOrLd'];
-
-const capitalize = (word) => word[0].toUpperCase() + word.slice(1).toLowerCase();
-
-const capitalizeWords = (words) =>  words.map((word) => capitalize(word));
-
-console.log(capitalizeWords(arrayStrings));
-
-
-//task №3
-
-const usersList = [
+const videos = [
 	{
-		id: 1,
-		name: 'Alex',
+	id: 65432445,
+	title: 'The Chamber',
 	},
 	{
-		id: 2,
-		name: 'Toma',
+		id: 675465,
+		title: 'Fracture',
 	},
-	{
-		id: 3,
-		name: 'Max',
-	},
-]
-
-const showUsers = (usersList, blackListId) => {
-	return usersList.filter((user) => !blackListId.includes(user.id));
-}
-
-console.log(showUsers(usersList, [1]));
-
-
-//task №4
-
-const forCheckPalindrome = (value) => value.split('').reverse().join('').toLowerCase() === value.toLowerCase();
-
-console.log(forCheckPalindrome('Шалаш шалаШ'));
-
-
-//task №5
-const users = [
-	{
-		id: 1,
-		first_name: 'Jeanette',
-		last_name: 'Penddreth',
-		email: 'jpenddreth0@census.gov',
-		gender: 'Female',
-		ip_address: '26.58.193.2',
-	},
-	{
-		id: 2,
-		first_name: 'Petr',
-		last_name: 'Jackson',
-		email: 'gfrediani1@senate.gov',
-		gender: 'Male',
-		ip_address: '229.179.4.212',
-	},
-];
-
-const devibeByGender = (users) => {
-
-	return users.reduce((accumulator, {first_name, last_name, ...otherFields}) => {
-		const userToPush = {
-			...otherFields,
-			fulName: `${first_name} ${last_name}`,
-		}
-
-		userToPush.gender === 'Male' ? accumulator.man.push(userToPush) : accumulator.women.push(userToPush);
-
-		return accumulator;
-
-	}, {man: [], women: []})
-}
-console.log(devibeByGender(users));
-
-
-//task №6
-const newReleases = [
 	{
 		id: 70111470,
-		title: 'Die Hard',
-		boxart: 'https://learn.javascript.ru/object1111',
-		uri: 'https://learn.javascript.ru/object2222',
-		rating: [5.0],
-		bookmark: [],
-	},
-	{
-		id: 1561615454,
 		title: 'Bad Boys',
-		boxart: 'https://learn.javascript.ru/object1111',
-		uri: 'https://learn.javascript.ru/object2222',
-		rating: [4.0],
-		bookmark: [{id: 23123123, time: 1232132}],
 	},
 	{
-		id: 7011142323270,
-		title: 'The Chamber',
-		boxart: 'https://learn.javascript.ru/object1111',
-		uri: 'https://learn.javascript.ru/object2222',
-		rating: [4.0],
-		bookmark: [],
-	},
-	{
-		id: 8729789470,
-		title: 'Fracture',
-		boxart: 'https://learn.javascript.ru/object1111',
-		uri: 'https://learn.javascript.ru/object2222',
-		rating: [5.0],
-		bookmark: [{id: 3123, time:32332}],
+		id: 654356453,
+		title: 'Die Hard',
 	},
 ];
 
-let mapReleases = (releases) => {
-	return releases.map(({id, title}) => ({id, title}));
+const videoTitleById = (videoItem) => videoItem.reduce((result, video) => {
+    return {
+      ...result,
+      [video.id]: video.title,
+    }
+  }, {});
+  
+console.log(videoTitleById(videos));
+
+
+//Task №2
+
+const boxarts = [
+  {
+    width: 200,
+    height: 200,
+    url: "https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/values",
+  },
+  {
+    width: 150,
+    height: 200,
+    url: "https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/toString",
+  },
+  {
+    width: 500,
+    height: 200,
+    url: "https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/assign",
+  },
+  {
+    width: 425,
+    height: 150,
+    url: "https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/valueOf",
+  },
+];
+
+//solutionNumberOne
+const arrayWithArea = boxarts.map((item) => {
+  return {
+    area : item.width * item.height,
+    url : item.url,
+  }
+});
+
+const getUrl = arrayWithArea.reduce((result, item) => {
+  return result.area > item.area ? result : item
+});
+
+console.log(`URL max square - ${getUrl.url}`);
+
+//solutionNumberTwo
+const showMaxUrlInArray = boxarts.reduce((result, item) => {
+  item.area = item.width * item.height;
+
+  return result.area > item.area ? result : item
+}, 0);
+
+console.log(`URL with max area - ${showMaxUrlInArray.url}`);
+
+
+//Task №3
+const arrayNumbersWithRepeats = [2, 2, 3, 2, 5, 9, 2, 2, 2, 7, 6, 2, 2, 2, 4];
+
+const objectWithRepeatValues = arrayNumbersWithRepeats.reduce((result, value) => {
+  result[value] = (result[value] || 0) + 1;
+  return result;
+}, {})
+
+const maxRepeatValue = Math.max(...Object.values(arrayNumbersWithRepeats));
+
+for(let key in objectWithRepeatValues) {
+  if(objectWithRepeatValues[key] === maxRepeatValue) {
+    console.log(`Max repeat: ${objectWithRepeatValues[key]}, value: ${key}`)
+  }
+};
+
+
+//Task №4
+
+//solutionNumberOne
+const objectWithValues = {a: 1, b: 'Hello', c: null, z: undefined};
+
+const fildFilterWithValue = (fildFilter) => {
+  for (let value in objectWithValues) {
+    if (objectWithValues[value] === null || objectWithValues[value] === undefined) {
+      delete objectWithValues[value];
+    }
+  }
+  return objectWithValues;
 }
 
-console.log(mapReleases(newReleases));
+console.log(fildFilterWithValue(objectWithValues));
+
+//solutionNumberTwo
+const resultFilter = Object.entries(objectWithValues).reduce((acc,[key,value]) => (
+  value == null ? acc : (acc[key] = value, acc)
+), {})
+
+console.log(resultFilter);
 
 
-//task №7
+//Task №5
 
-let ids = (releases) => {
+//solutionNumberOne
+const arrayNumbers = [1, 1, 2, 3, 3];
 
-	return releases.reduce((accumulator, {id, rating}) => {
+const objectWithRepeat = arrayNumbers.reduce((result, value) => {
+  result[value] = (result[value] || 0) + 1;
+  return result;
+}, {})
 
-		if (rating[0] === 5) {
-			accumulator.push(id);
-		}
+for(let key in objectWithRepeat) {
+  if(objectWithRepeat[key] ===  1) {
+    console.log(`Min repeat: ${objectWithRepeat[key]}, value: ${key}`)
+  }
+};
 
-		return accumulator;
+//solutionNumberTwo
+let result;
+for(let i of arrayNumbers)
+{
+  if(arrayNumbers.indexOf(i) === arrayNumbers.lastIndexOf(i)) result = i;
+}
+console.log(result);
 
-	}, [])
+
+//Task №6
+
+const cutString = (string, maxLength) => {
+  if (string.length > maxLength) {
+    string = string.slice(0, maxLength) + '...';
+  }
+  return string;
+}
+console.log(cutString('hello', 3));
+
+
+//Task №7
+
+const createPromise = (number) => {
+  return new Promise(resolve => {
+    setTimeout(() =>{ resolve(number)}, 500)
+  })
 }
 
-console.log(ids(newReleases));
+const someFn = async () => {
+	const data = await createPromise(5);
+  console.log(data);
+}
+someFn()
+
+//
+setTimeout(function(){console.log("Hello")},1000);
+
+//
+function waitforme(milisec) {
+  return new Promise(resolve => {
+      setTimeout(() => { resolve('') }, milisec);
+  })
+}
+
+async function printy() {
+  for (let i = 0; i < 10; ++i) {
+      await waitforme(1000);
+      console.log(i);
+  }
+  console.log("Loop execution finished!)");
+}
+
+printy();
+
+
+//
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+(async () => {
+  await delay(1000);
+  console.log(1);
+  await delay(5000);
+  console.log(2);
+})();
