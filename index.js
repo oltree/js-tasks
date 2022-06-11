1b26c3a200e2baf52916d6e9ca9899ceb6868cc8
oltree <o.meleh10@gmail.com>
2022-06-11 14:46:22 +0300

    HW

--- index.js
+++ index.js
@@ -1,65 +1,33 @@
-/* const getPokemons = async () => {
-  const BASE_URL = `https://pokeapi.co/api/v2/pokemon`;
-  try {
-    const data = await fetch(BASE_URL).then((response) => response.json());
+const BASE_URL_1 = "https://jsonplaceholder.typicode.com/users";
+const BASE_URL_2 = "https://jsonplaceholder.typicode.com/todos";
 
-    const urls = [];
-    for (let i = 1; i <= data.results.length; i++) {
-      urls.push(
-        await fetch(`${BASE_URL}/${i}`).then((response) => response.json())
-      );
-    }
-
-    const pokemonsMap = Promise.all(urls);
-
-    for (let i = 0; i <= urls.length - 1; i++) {
-      urls[i].stats = urls[i].stats.reduce((result, { base_stat, stat }) => {
-        result[stat.name] = base_stat;
-
-        return result;
-      }, {});
-    }
-
-    return pokemonsMap;
-  } catch (error) {}
+const dataUsers = async () => {
+  const result = await fetch(BASE_URL_1).then((response) => response.json());
+  return result;
 };
 
-console.log(getPokemons()); */
+const dataTodos = async () => {
+  const result = await fetch(BASE_URL_2).then((response) => response.json());
+  return result;
+};
 
-const InfoAboutPokemons = async () => {
-  const BASE_URL = `https://pokeapi.co/api/v2/pokemon`;
+const agrigateUsersTodos = async () => {
   try {
-    const pokemonsData = await fetch(BASE_URL).then((response) =>
-      response.json()
-    );
+    const usersList = await dataUsers();
 
-    const arrayPromisesWithPokemons = (pokemonsData) => {
-      return pokemonsData.map(({ url }) =>
-        fetch(url).then((response) => response.json())
-      );
-    };
+    const todosList = await dataTodos();
 
-    const pokemonsMap = await Promise.all(
-      arrayPromisesWithPokemons(pokemonsData.results)
-    );
-
-    const modifiedPokemonsMap = (pokemonsMap) => {
-      return pokemonsMap.map(({ stats, ...otherFields }) => {
-        return {
-          ...otherFields,
-          stats: (stats = stats.reduce((result, { base_stat, stat }) => {
-            result[stat.name] = base_stat;
-
-            return result;
-          }, {})),
-        };
-      });
-    };
-
-    return modifiedPokemonsMap(pokemonsMap);
+    return usersList.map((user) => {
+      return {
+        ...user,
+        todos: todosList.filter(({ userId }) => {
+          return userId === user.id;
+        }),
+      };
+    });
   } catch (error) {
-    console.error("Error 100500!");
+    console.log(error.message);
   }
 };
 
-console.log(InfoAboutPokemons());
+agrigateUsersTodos().then((data) => console.log(data));
