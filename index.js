d971ae1e7fea7d612e683c1408dc994a27e338f7
oltree <o.meleh10@gmail.com>
2022-08-03 01:18:07 +0300

    tasks JS

--- index.js
+++ index.js
@@ -1,54 +1,68 @@
-const BASE_URL_1 = "https://jsonplaceholder.typicode.com/users";
-const BASE_URL_2 = "https://jsonplaceholder.typicode.com/todos";
+//Task 1: finding the sum of digits
+const sumDigints = (stringifyNumber) => {
+  const arrayDigits = stringifyNumber.split("");
 
-const dataUsers = fetch(BASE_URL_1).then((response) => response.json());
+  const sumValue = arrayDigits.reduce(
+    (accumulator, value) => accumulator + +value,
+    0
+  );
 
-const dataTodos = fetch(BASE_URL_2).then((response) => response.json());
-
-//solution number 1
-
-/* const agrigateUsersTodos = async () => {
-  try {
-    const [usersList, todosList] = await Promise.all([dataUsers, dataTodos]);
-
-    return usersList.map((user) => {
-      return {
-        ...user,
-        todos: todosList.filter(({ userId }) => userId === user.id),
-      };
-    });
-  } catch (error) {
-    console.log(error.message);
+  if (sumValue > 9) {
+    return sumDigints(sumValue.toString());
+  } else {
+    return sumValue;
   }
 };
+//console.log(sumDigints("88"));
 
-agrigateUsersTodos().then((data) => console.log(data)); */
+//Task 2: removing characters from a string
+const removeChars = (string, length) => {
+  if (string.length < length) return string;
 
-//solution number 2
-
-const agrigateUsersTodos = async () => {
-  try {
-    const [usersList, todosList] = await Promise.all([dataUsers, dataTodos]);
-
-    const todosMap = todosList.reduce((acc, todo) => {
-      if (!acc[todo.userId]) {
-        acc[todo.userId] = [];
-      } else {
-        acc[todo.userId].push(todo);
-      }
-
-      return acc;
-    }, {});
-
-    return usersList.map((item) => {
-      return {
-        ...item,
-        todos: todosMap[item.id],
-      };
-    });
-  } catch (error) {
-    console.log(error.message);
-  }
+  return string.slice(length);
 };
+//console.log(removeChars("Hello World!", 1));
 
-agrigateUsersTodos().then((data) => console.log(data));
+//Task 3: insert '-' in string
+const insertSymbolsInString = (string) => {
+  //return string.split(" ").join("-");
+  return string.replace(/\s/g, "-");
+};
+//console.log(insertSymbolsInString("Hello World!"));
+
+//Task 4: StartCase
+const stringInStartCase = (string) => {
+  return string
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+};
+//console.log(stringInStartCase("start case"));
+
+//Task 5: case change to reverse
+const caseChange = (string) => {
+  const arrayWords = string.split("");
+
+  const result = arrayWords.map((char) => {
+    if (char === char.toUpperCase()) {
+      return char.toLowerCase();
+    } else {
+      return char.toUpperCase();
+    }
+  });
+
+  return result.join("");
+};
+//console.log(caseChange("hEllO WorLD"));
+
+//Task 6: insensitive search
+const insensitiveSearch = (string1, string2) => {
+  return string1.includes(string2);
+};
+//console.log(insensitiveSearch("hello my friend", "my"));
+
+//Task 7: get filename
+const getPathName = (string) => {
+  return string.split("/").pop();
+};
+//console.log(getPathName("/home/user/dir/file.txt"));
